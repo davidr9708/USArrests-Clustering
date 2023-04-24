4f556c6a41f75813449e7cc1ed6c69f17b4f7879
Daniel Rodriguez <71290957+davidr9708@users.noreply.github.com>
2023-04-24 12:09:01 -0500

    Add files via upload

--- Code/USArrests.R
+++ Code/USArrests.R
@@ -1,58 +1,82 @@
-# Load the required libraries
-library(tidyverse)
-library(cluster) # For k-means clustering
-library(factoextra) # For visualizations
-library(ggrepel)
+# 0. Load the required libraries
+library(pacman)
 
-# Extract and clean
+p_load(tidyverse, 
+       cluster, 
+       factoextra,
+       ggrepel,
+       rstatix
+       )
+
+# 1. Extract and clean
 data(USArrests)
-raw_data    <- USArrests[,c(1,2,4)] ## Select relevant features
+raw_data    <- USArrests[,1:4] ## Select relevant features
 scaled_data <- scale(raw_data) ## Scale the data
 
-# Verify extreme outliers
-raw_data %>% 
-  identify_outliers(c(Rape, Assault))
-# Apply k-means clustering
-k_clusters <- 3 # Number of clusters
-kmeans_result <- kmeans(scaled_data                , 
-                        algorithm = "Hartigan-Wong",
-                        centers   = k_clusters     , 
-                        nstart    = 1000
-                        ) # K-means clustering  
+# 2. Analysis
+## 2.1 Verify extreme outliers
+    for (name in colnames(scaled_data)) {
+        outliers <- raw_data %>% 
+            identify_outliers(name)
+       
+        cat('Variable ', name, ':', '\n')
+        cat("   No. extreme outliers:", length(outliers$outliersExtreme), "\n")
+        cat("   No. outliers:", length(outliers$outliers), "\n")
+        
+    }
 
+## 2.2 Apply k-means clustering
+    k_clusters <- 3 # Number of clusters
+    kmeans_result <- kmeans(scaled_data                , 
+                            algorithm = "Hartigan-Wong",
+                            centers   = k_clusters     , 
+                            nstart    = 1000
+                            ) # K-means clustering  
+    
+    
+    raw_data$cluster <- as.factor(kmeans_result$cluster) # Add the cluster label
 
-raw_data$cluster <- as.factor(kmeans_result$cluster) # Add the cluster label
+## 2.3 Visualize clusters
+    fviz_cluster(kmeans_result, 
+                 data = scaled_data,
+                 geom = "point",
+                 ellipse.type = "norm",
+                 stand = FALSE,
+                 ellipse.level = 0.95, 
+                 show.clust.cent = TRUE
+                 ) +
+    geom_text_repel(aes(color = cluster, 
+                        label = row.names(scaled_data)
+                        ), 
+                    size = 3, 
+                    show.legend = FALSE
+                    ) + 
+    theme(panel.background = element_blank()
+    ) ## Graph Principal component analysis of the clusters
 
-# Visualize clusters
-## Graph
-fviz_cluster(kmeans_result, 
-             data = scaled_data,
-             geom = "point",
-             ellipse.type = "norm",
-             stand = FALSE,
-             ellipse.level = 0.95, 
-             show.clust.cent = TRUE
-             ) + 
-  theme(panel.background = element_blank()
-        ) +
-  geom_text_repel(aes(color = cluster, 
-                      label = row.names(scaled_data)), 
-                  size = 3, 
-                  show.legend = FALSE)
+    
+    aggregate(x  = USArrests, 
+              by = list(cluster = kmeans_result$cluster
+                        ), 
+              FUN = mean
+              ) ## Show the the means of each cluster
 
-## Dataset
-aggregate(x  = USArrests, 
-          by = list(cluster = kmeans_result$cluster
-                    ), 
-          FUN = mean
-          ) 
+# 3. Export data
+if (!file.exists("Results")) {
+  # Create a new directory if it doesn't exist
+  dir.create("Results")
+  cat("Directory created successfully.\n")
+} else {
+  cat("Directory already exists.\n")
+} # Verify the existence of the directory
+    
+save(raw_data, 
+     file = 'Results/Results.Rdata'
+     ) # To save as Rdata
 
-# Export data
-save(raw_data, file = 'Results/Results.Rdata')
-
-# Exporting the data to visualize in tableau
-write.csv(file ='Clustering_result.csv',  raw_data)
-
+write.csv(raw_data,
+          file ='Clustering_result.csv'
+          ) # Exporting the data to visualize in tableau
 
 
 # Interpretation
